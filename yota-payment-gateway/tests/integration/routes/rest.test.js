c435bb78f1f76204edc3a4e2b1d93ad5e9e99ee9
Arootin Aghazaryan <arootin.aghazaryan@dataart.com>
2020-07-02 21:25:47 +0400

    fix rest.test.js

--- yota-payment-gateway/tests/integration/routes/rest.test.js
+++ yota-payment-gateway/tests/integration/routes/rest.test.js
@@ -1,9 +1,13 @@
 const request = require('supertest');
+const connectionPool = require('../../../infrastructure/db/connectionpool');
 const app = require('../../../app');
 
 jest.setTimeout(20000);
 
 describe('Not existing routes', () => {
+  beforeAll(async () => {
+    await connectionPool.init();
+  });
   test('Not Found - 404', async () => {
     const response = await request(app).get('/notimplementedroute').expect(404);
     expect(response.body.message).toBeTruthy();
