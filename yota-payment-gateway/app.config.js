bfe27a7316e6b7bb2da39828513a241d8a84a19a
Arootin Aghazaryan <arootin.aghazaryan@dataart.com>
2020-07-04 13:01:37 +0400

    Make payment acceptance configurable

--- yota-payment-gateway/app.config.js
+++ yota-payment-gateway/app.config.js
@@ -5,7 +5,11 @@ module.exports = {
   dbConfig: {
     user: process.env.NODE_ORACLEDB_USER || 'yotaott',
     password: process.env.NODE_ORACLEDB_PASSWORD || 'yota0TT',
-    connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || 'localhost:49161/xe',
-    externalAuth: !!process.env.NODE_ORACLEDB_EXTERNALAUTH
-  }
+    connectString:
+      process.env.NODE_ORACLEDB_CONNECTIONSTRING || 'localhost:49161/xe',
+    externalAuth: !!process.env.NODE_ORACLEDB_EXTERNALAUTH,
+  },
+  application: {
+    acceptClosedAccountPayments: true,
+  },
 };
